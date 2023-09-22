062f8878bbfe9141d700926196cb4dca34708d1f
palloncino <antonio.guiotto.dev@gmail.com>
2023-09-22 13:18:32 +0200

    updated aws credentials + script

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -14,15 +14,15 @@ fi
 
 
 echo "Removing all files on bucket"
-aws s3 rm s3://${BUCKET} --recursive
+aws s3 rm s3://${BUCKET} --recursive --profile=a1
 
 
 echo "Attempting to upload site .."
 echo "Command:  aws s3  sync $SOURCE_DIR s3://$BUCKET/"
-aws s3 sync ${SOURCE_DIR} s3://${BUCKET}/
+aws s3 sync ${SOURCE_DIR} s3://${BUCKET}/ --profile=a1
 echo "S3 Upload complete"
 
 echo "Invalidating cloudfrond distribution to get fresh cache"
-aws cloudfront create-invalidation --distribution-id=E2EQL0AAS6JARG --paths / --profile=a3
+aws cloudfront create-invalidation --distribution-id=E35044XML0GVE1 --paths / --profile=a1
 
-echo "Deployment complete" 
+echo "Deployment complete"
